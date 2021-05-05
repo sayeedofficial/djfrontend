0ccb16f662a8cb54f33cca8482279fbdd3add0bf
Mohammed Sayeed <mdsayeed6220@gmail.com>
2021-05-05 23:20:25 +0530

    Added components

---
+++ components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Link from "next/link";
+const Header = () => {
+  return (
+    <div>
+      <h1>This is Header</h1>
+    </div>
+  );
+};
+
+export default Header;
